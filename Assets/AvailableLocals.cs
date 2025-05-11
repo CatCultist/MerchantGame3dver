91039526cfe7722a5b621a9dc05953166c533463
CatCultist <infinitesnakes@gmail.com>
2025-05-11 13:12:43 +0100

    MapStuff

---
+++ Assets/AvailableLocals.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class AvailableLocals : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject[] Close= new GameObject[3];
+
+}
