8d77ece4e014110a03ee04bc9e0da37afb649164
CatCultist <infinitesnakes@gmail.com>
2025-05-12 19:24:56 +0100

    fixes

--- Assets/Map/ToMap.cs
+++ Assets/Map/ToMap.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ToMap : MonoBehaviour
 {
     public int ExitNumber;
+    public Object MapScene;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnTriggerEnter(Collider other)
     {
         PlayerPrefs.SetInt("Exit", ExitNumber);
+        SceneManager.LoadScene(MapScene.name);
     }
     void Start()
     {
