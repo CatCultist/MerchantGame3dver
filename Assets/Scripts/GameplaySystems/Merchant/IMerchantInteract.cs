ff2aac1a4787067d695b127ab38655567e594538
CatCultist <infinitesnakes@gmail.com>
2025-05-12 17:09:07 +0100

    Revert "weird merge"
    This reverts commit ab71b7c429d972061837fa30680ab2120f3b4163, reversing
    changes made to 3cfa2432ab65bbeba8c1faa743b6629999cc0370.

--- Assets/Scripts/GameplaySystems/Merchant/IMerchantInteract.cs
+++ Assets/Scripts/GameplaySystems/Merchant/IMerchantInteract.cs
@@ -2,13 +2,8 @@ namespace GameplaySystems.Merchant
 {
     public interface IMerchantInteract
     {
-<<<<<<< HEAD
         public void OnItemPurchase(string itemID, int quantity, float itemPrice, float playerPrice);
         public void OnItemSold(string itemID, int quantity, float itemPrice, float playerPrice);
-=======
-        public void OnItemPurchase(TradeGoods.TradeGoods tradeGood, string itemID, int quantity, float itemPrice, float playerPrice);
-        public void OnItemSold(TradeGoods.TradeGoods tradeGood, string itemID, int quantity, float itemPrice, float playerPrice);
->>>>>>> ciaran
         public void OnWeekPay();
     }
 }
